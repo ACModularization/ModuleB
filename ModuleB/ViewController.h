9d9471cc7afbb5ed246f6e333f0d8a5faa65caad
张亚浩 <zhangyahao897@pingan.com.cn>
2019-03-26 17:48:45 +0800

    config

---
+++ ModuleB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModuleB
+//
+//  Created by 张亚浩 on 2019/3/22.
+//  Copyright © 2019 Ace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
